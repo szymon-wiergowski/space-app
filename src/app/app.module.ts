25f97484ccf2ba287767c4878144c1265a05cb23
swiergowski <swiergowski@gwo.pl>
2021-07-09 15:50:56 +0200

    mission 12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { A2sCommModule } from 'a2s-comm';
 import { SpaceModule } from './space/space.module';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
+import { BlackHoleComponent } from './black-hole/black-hole.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, A2sCommModule, SpaceModule, HttpClientModule, SharedModule],
+  declarations: [AppComponent, BlackHoleComponent],
+  imports: [BrowserModule, SpaceModule, AppRoutingModule, A2sCommModule, HttpClientModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/space/space-routing.module.ts
+++ src/app/space/space-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HangarComponent } from './hangar/hangar.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'space', component: HangarComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SpaceRoutingModule { }
+export class SpaceRoutingModule {}
