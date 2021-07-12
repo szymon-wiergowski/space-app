7792e2607334d4767951c0040a2f3393ac89388e
swiergowski <swiergowski@gwo.pl>
2021-07-12 07:45:27 +0200

    mission 13 done

--- src/app/space/destruction.guard.ts
+++ src/app/space/destruction.guard.ts
@@ -1,31 +1,23 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanDeactivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { SpaceShipService } from './space-ship.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class DestructionGuard implements CanActivate, CanActivateChild, CanDeactivate<unknown>, CanLoad {
+export class DestructionGuard implements CanActivate {
+  constructor(private spaceShipService: SpaceShipService, private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-  canActivateChild(
-    childRoute: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-  canDeactivate(
-    component: unknown,
-    currentRoute: ActivatedRouteSnapshot,
-    currentState: RouterStateSnapshot,
-    nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const hasSpaceShips = this.spaceShipService.hangarShips.getValue().length > 0;
+    if (hasSpaceShips) {
+      return true;
+    }
+
+    alert('Nie ma statków w hangarze!');
+    return this.router.parseUrl('/');
   }
 }
